b56d3a651afcfbb0d477e625fc711a21e8124459
Mayel <git@mayel.space>
2021-04-03 15:25:23 +0200

    cleanup

--- lib/social/follows.ex
+++ lib/social/follows.ex
@@ -4,19 +4,20 @@ defmodule Bonfire.Social.Follows do
   alias Bonfire.Data.Social.Follow
   alias Bonfire.Social.FeedActivities
   alias Bonfire.Social.Activities
+  import Bonfire.Common.Utils
 
   use Bonfire.Repo.Query,
     schema: Follow,
     searchable_fields: [:id, :follower_id, :followed_id],
     sortable_fields: [:id]
 
-  def following?(%User{}=user, followed), do: not is_nil(get!(user, followed))
-  def get(%User{}=user, followed), do: repo().single(by_both_q(user, followed))
-  def get!(%User{}=user, followed), do: repo().one(by_both_q(user, followed))
-  def by_follower(%User{}=user), do: repo().all(followed_by_follower_q(user))
-  # def by_follower(%User{}=user), do: repo().all(by_follower_q(user))
-  def by_followed(%User{}=user), do: repo().all(by_followed_q(user))
-  def by_any(%User{}=user), do: repo().all(by_any_q(user))
+  def following?(user, followed), do: not is_nil(get!(user, followed))
+  def get(user, followed), do: repo().single(by_both_q(user, followed))
+  def get!(user, followed), do: repo().one(by_both_q(user, followed))
+  def by_follower(user), do: repo().all(followed_by_follower_q(user))
+  # def by_follower(user), do: repo().all(by_follower_q(user))
+  def by_followed(user), do: repo().all(by_followed_q(user))
+  def by_any(user), do: repo().all(by_any_q(user))
 
   defp list(filters, current_user) do
     # TODO: check permissions
@@ -37,81 +38,78 @@ defmodule Bonfire.Social.Follows do
     |> repo().all
   end
 
-  def follow(%User{} = follower, %{} = followed) do
+  def follow(%{} = follower, %{} = followed) do
     with {:ok, follow} <- create(follower, followed) do
       # FeedActivities.publish(follower, :follow, followed)
       FeedActivities.maybe_notify_object(follower, :follow, followed)
       {:ok, follow}
     end
   end
-  def follow(%User{} = user, object) when is_binary(object) do
+  def follow(user, object) when is_binary(object) do
     with {:ok, object} <- Bonfire.Common.Pointers.get(object) do
       follow(user, object)
     end
   end
 
-  def unfollow(%User{}=follower, %{}=followed) do
+  def unfollow(follower, %{}=followed) do
     delete_by_both(follower, followed)
     Activities.delete_by_subject_verb_object(follower, :follow, followed) # delete the like activity & feed entries
   end
-  def unfollow(%User{} = user, object) when is_binary(object) do
+  def unfollow(%{} = user, object) when is_binary(object) do
     with {:ok, object} <- Bonfire.Common.Pointers.get(object) do
       unfollow(user, object)
     end
   end
 
-  defp create(%{} = follower, %{} = followed) do
+  defp create(follower, followed) do
     changeset(follower, followed) |> repo().insert()
   end
 
-  defp changeset(%{id: follower}, %{id: followed}) do
-    Follow.changeset(%Follow{}, %{follower_id: follower, followed_id: followed})
+  defp changeset(follower, followed) do
+    Follow.changeset(%Follow{}, %{follower_id: ulid(follower), followed_id: ulid(followed)})
   end
 
   @doc "Delete Follows where i am the follower"
-  defp delete_by_follower(%User{}=me), do: do_delete(by_follower_q(me))
+  defp delete_by_follower(me), do: do_delete(by_follower_q(me))
 
   @doc "Delete Follows where i am the followed"
-  defp delete_by_followed(%User{}=me), do: do_delete(by_followed_q(me))
+  defp delete_by_followed(me), do: do_delete(by_followed_q(me))
 
   @doc "Delete Follows where i am the follower or the followed."
-  defp delete_by_any(%User{}=me), do: do_delete(by_any_q(me))
+  defp delete_by_any(me), do: do_delete(by_any_q(me))
 
   @doc "Delete Follows where i am the follower and someone else is the followed."
-  defp delete_by_both(%User{}=me, %{}=followed), do: do_delete(by_both_q(me, followed))
+  defp delete_by_both(me, followed), do: do_delete(by_both_q(me, followed))
 
   defp do_delete(q), do: elem(repo().delete_all(q), 1)
 
-  def by_follower_q(%User{id: id}) do
+  def by_follower_q(id) do
     from f in Follow,
-      where: f.follower_id == ^id,
+      where: f.follower_id == ^ulid(id),
       select: f.id
   end
 
-  def followed_by_follower_q(%User{id: id}) do
+  def followed_by_follower_q(id) do
     from f in Follow,
-      where: f.follower_id == ^id,
+      where: f.follower_id == ^ulid(id),
       select: f.followed_id
   end
 
-  def by_followed_q(%User{id: id}) do
+  def by_followed_q(id) do
     from f in Follow,
-      where: f.followed_id == ^id,
+      where: f.followed_id == ^ulid(id),
       select: f.id
   end
 
-  def by_any_q(%User{id: id}) do
+  def by_any_q(id) do
     from f in Follow,
-      where: f.follower_id == ^id or f.followed_id == ^id,
+      where: f.follower_id == ^ulid(id) or f.followed_id == ^ulid(id),
       select: f.id
   end
 
-  def by_both_q(%User{id: follower}, followed), do: by_both_q(follower, followed)
-  def by_both_q(follower, %{id: followed}), do: by_both_q(follower, followed)
-
-  def by_both_q(follower, followed) when is_binary(follower) and is_binary(followed) do
+  def by_both_q(follower, followed) do
     from f in Follow,
-      where: f.follower_id == ^follower and f.followed_id == ^followed,
+      where: f.follower_id == ^ulid(follower) and f.followed_id == ^ulid(followed),
       select: f.id
   end
 
--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -6,16 +6,6 @@ defmodule Bonfire.Social.Test.FakeHelpers do
   import ExUnit.Assertions
 
   import Bonfire.Social.Integration
-
-  def fake_account!(attrs \\ %{}) do
-    cs = Accounts.signup_changeset(Fake.account(attrs))
-    assert {:ok, account} = repo().insert(cs)
-    account
-  end
-
-  def fake_user!(%Account{}=account, attrs \\ %{}) do
-    assert {:ok, user} = Users.create(Fake.user(attrs), account)
-    user
-  end
+  import Bonfire.Me.Fake
 
 end
