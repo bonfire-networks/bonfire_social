7b0d1894d1829a1ca15301f1d71bdad97846175d
Mayel <git@mayel.space>
2021-04-15 22:33:25 +0200

    tests

--- test/social/mentions_test.exs
+++ test/social/mentions_test.exs
@@ -4,33 +4,36 @@ defmodule Bonfire.Social.MentionsTest do
   alias Bonfire.Social.Posts
   alias Bonfire.Me.Fake
 
-  test "mention works" do
+  test "can post with a mention" do
 
     me = Fake.fake_user!()
     mentioned = Fake.fake_user!()
-    assert {:ok, mention} = Posts.publish(user, attrs)
-    #IO.inspect(mention)
-    assert mention.mentioner_id == me.id
-    assert mention.mentioned_id == mentioned.id
+    attrs = %{post_content: %{html_body: "<p>hey @#{mentioned.character.username} you have an epic html message</p>"}}
+    assert {:ok, mention} = Posts.publish(me, attrs)
+    IO.inspect(mention)
+    assert mention.activity.activity.object.created.creator_id == me.id
+    # assert mention.mentioned_id == mentioned.id # todo check if received
   end
 
-  test "can fetch mentions" do
-    me = Fake.fake_user!()
-    mentioned = Fake.fake_user!()
-    assert {:ok, mention} = Posts.publish(user, attrs)
+  # test "can fetch mentions" do
+  #   me = Fake.fake_user!()
+  #   mentioned = Fake.fake_user!()
+  #   attrs = %{post_content: %{html_body: "<p>hey @#{mentioned.character.username} you have an epic html message</p>"}}
+  #   assert {:ok, mention} = Posts.publish(me, attrs)
 
-    assert {:ok, fetched_mention} = Posts.read(me, mentioned)
+  #   assert {:ok, fetched_mention} = Posts.read(me, mentioned)
 
-    assert fetched_mention == mention.id
-  end
+  #   assert fetched_mention == mention.id
+  # end
 
-  test "can check if mentioning someone" do
-    me = Fake.fake_user!()
-    mentioned = Fake.fake_user!()
-    assert {:ok, mention} = Posts.publish(user, attrs)
+  # test "can check if mentioning someone" do
+  #   me = Fake.fake_user!()
+  #   mentioned = Fake.fake_user!()
+  #   attrs = %{post_content: %{html_body: "<p>hey @#{mentioned.character.username} you have an epic html message</p>"}}
+  #   assert {:ok, mention} = Posts.publish(me, attrs)
 
-    # assert true == Mentions.mentioning?(me, mentioned)
-  end
+  #   # assert true == Mentions.mentioning?(me, mentioned)
+  # end
 
 
 end
