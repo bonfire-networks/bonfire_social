736f1b2651ff0d140b52b89d36a12521356c56c4
Mayel <git@mayel.space>
2021-05-20 14:35:11 +0200

    add emojis

--- lib/social/post_contents.ex
+++ lib/social/post_contents.ex
@@ -1,22 +1,35 @@
 defmodule Bonfire.Social.PostContents do
   alias Bonfire.Data.Social.PostContent
+  alias Bonfire.Common.Extend
 
   def prepare_content(attrs, text \\ nil)
   def prepare_content(%{post_content: %{} = attrs}, text), do: prepare_content(attrs, text)
   def prepare_content(%{post: %{} = attrs}, text), do: prepare_content(attrs, text)
   def prepare_content(attrs, text) when is_binary(text) and bit_size(text) > 0 do
-    # use text overide if provided
-    Map.merge(attrs, %{html_body: text})
+    # use seperate text param if provided
+    Map.merge(attrs, %{html_body: prepare_text(text), name: prepare_text(Map.get(attrs, :name)), summary: prepare_text(Map.get(attrs, :summary))})
   end
-  def prepare_content(%{summary: summary, html_body: body} = attrs, _) when (is_nil(body) or body=="") and not (is_nil(summary) or summary=="")  do
+  def prepare_content(%{summary: summary, html_body: body} = attrs, _) when (not is_binary(body) or body=="") and not (is_nil(summary) or summary=="") do
     # use summary as body if no body entered
-    Map.merge(attrs, %{html_body: summary, summary: ""})
+    Map.merge(attrs, %{html_body: prepare_text(summary), summary: nil, name: prepare_text(Map.get(attrs, :name))})
   end
-  def prepare_content(%{name: name, html_body: body} = attrs, _) when (is_nil(body) or body=="") and not (is_nil(name) or name=="") do
+  def prepare_content(%{name: name, html_body: body} = attrs, _) when (not is_binary(body) or body=="") and not (is_nil(name) or name=="") do
     # use title as body if no body entered
-    Map.merge(attrs, %{html_body: name, name: ""})
+    Map.merge(attrs, %{html_body: prepare_text(name), name: nil, summary: prepare_text(Map.get(attrs, :summary))})
+  end
+  def prepare_content(%{} = attrs, _) do
+    Map.merge(attrs, %{html_body: prepare_text(Map.get(attrs, :html_body)), name: prepare_text(Map.get(attrs, :name)), summary: prepare_text(Map.get(attrs, :summary))})
   end
   def prepare_content(attrs, _), do: attrs
+
+  def prepare_text(text) when is_binary(text) do
+    if Extend.module_enabled?(Emote) do
+      Emote.convert_text(text)
+    else
+      text
+    end
+  end
+  def prepare_text(other), do: other
 
 
   def indexing_object_format(%{post_content: obj}), do: indexing_object_format(obj)
