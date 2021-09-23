2fa660a81139794a36e7933c30f8483b53397b4c
Karen Konou <konoukaren@gmail.com>
2021-09-23 12:59:53 +0200

    Update fallback activity context

--- lib/social/apactivities.ex
+++ lib/social/apactivities.ex
@@ -2,8 +2,21 @@ defmodule Bonfire.Social.APActivities do
   alias Bonfire.Data.Social.APActivity
   alias Bonfire.Social.FeedActivities
 
+  require Logger
+
   import Bonfire.Common.Utils
   import Bonfire.Common.Config, only: [repo: 0]
+
+  def create(activity, object, nil) do
+    case Bonfire.Me.Users.ActivityPub.get_actor_by_ap_id(activity.data["actor"]) do
+      {:ok, actor} ->
+        create(activity, object, actor)
+      _ ->
+        error = "cannot create fallback activity with nil actor"
+        Logger.error(error)
+        {:error, error}
+    end
+  end
 
   def create(activity, object, actor) do
     json =
