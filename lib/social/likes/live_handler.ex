9d9df63df3768f7acc7bfcea557bf42c2b54c3bb
Mayel de Borniol <git@mayel.space>
2022-05-02 19:56:54 +1200

    Fixes

--- lib/social/likes/live_handler.ex
+++ lib/social/likes/live_handler.ex
@@ -52,7 +52,8 @@ defmodule Bonfire.Social.Likes.LiveHandler do
 
   def preload(list_of_assigns) do
     current_user = current_user(List.first(list_of_assigns))
-    # |> debug("current_user")
+    |> info("current_user")
+
     # debug(list_of_assigns, "list of assign:")
     list_of_objects = list_of_assigns
     |> Enum.map(& e(&1, :object, nil))
@@ -65,7 +66,8 @@ defmodule Bonfire.Social.Likes.LiveHandler do
     # |> debug("list_of_ids")
 
     my_states = if current_user, do: Bonfire.Social.Likes.get!(current_user, list_of_ids, preload: false) |> Map.new(fn l -> {e(l, :edge, :object_id, nil), true} end), else: %{}
-    # debug(my_states, "my_likes")
+
+    info(my_states, "my_likes")
 
     objects_counts = list_of_objects |> Map.new(fn o -> {e(o, :id, nil), e(o, :like_count, :object_count, nil)} end)
     # |> debug("like_counts")
