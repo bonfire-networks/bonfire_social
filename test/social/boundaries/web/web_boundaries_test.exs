633a3d3e0fea1512d5af6b201e40c99e75088872
ivanminutillo <ivanminutillo@outlook.it>
2023-04-12 16:13:55 +0200

    refactor

--- test/social/boundaries/web/web_boundaries_test.exs
+++ test/social/boundaries/web/web_boundaries_test.exs
@@ -6,11 +6,14 @@ defmodule Bonfire.Social.WebBoundariesTest do
   alias Bonfire.Social.{Posts, Follows, Likes, Boosts}
   alias Bonfire.Social.FeedActivities
   alias Bonfire.Boundaries
+  import Plug.Conn
+  import Phoenix.ConnTest
   # import Plug.Conn
   # import Phoenix.ConnTest
   # @endpoint MyEndpoint
+  @endpoint Application.compile_env!(:bonfire, :endpoint_module)
 
-  test "creating a post with a 'public' boundary and verify that all users can see and interact with it", %{conn: conn} do
+  test "creating a post with boundaries public and verify that all users can see and interact with it" do
     account = fake_account!()
     # Given a user
     alice = fake_user!(account)
@@ -26,55 +29,69 @@ defmodule Bonfire.Social.WebBoundariesTest do
         boundary: "public"
       }
     }
-    {:ok, view, html} = live(conn, "/feed")
+    {:ok, post} = Posts.publish(current_user: bob, post_attrs: attrs)
 
-    assert {:ok, post} = Posts.publish(current_user: bob, post_attrs: attrs)
+    {:ok, view, _html} = live(conn, "/feed")
+    IO.inspect(view)
+    # get the id of the <article> element
+    id = element(view, "#post-#{post.id}") |> Floki.attribute("id") |> debug("id is this one:")
 
 
-  end
+    # assign the created activity to element
+    activity = element(view, "#activity-#{post.id}")
 
-  test "creating a post with a 'local' boundary and verify that only users from that instance can see and interact with it." do
 
-  end
+    # Then I should see the post in my feed
+    assert view.assigns.feed_posts |> Enum.any?(&(&1.id == post.id))
+    # Then I should like the post
+    # Then I should boost the post
+    # Then I should comment on the post
 
-  test "creating a post with a 'mention' boundary and verify that only mentioned users can see and interact with it." do
-
-  end
-
-  test "Test creating a post with a 'custom' boundary and verify that only specified users or circles can see and interact with it according to their assigned roles." do
-
-  end
-
-  test "Test adding a user with a 'see' role and verify that the user can see the post but not interact with it." do
 
   end
 
-  test "adding a user with a 'read' role and verify that the user can read the post's content but not interact with it." do
+  # test "creating a post with a 'local' boundary and verify that only users from that instance can see and interact with it." do
 
-  end
+  # end
 
-  test "adding a user with an 'interact' role and verify that the user can like and boost the post." do
+  # test "creating a post with a 'mention' boundary and verify that only mentioned users can see and interact with it." do
 
-  end
+  # end
 
-  test "adding a user with a 'participate' role and verify that the user can engage in the post's activities and discussions." do
+  # test "Test creating a post with a 'custom' boundary and verify that only specified users or circles can see and interact with it according to their assigned roles." do
 
-  end
+  # end
 
-  test "adding a user with a 'caretaker' role and verify that the user can delete the post" do
+  # test "Test adding a user with a 'see' role and verify that the user can see the post but not interact with it." do
 
-  end
+  # end
 
-  test "adding a user with a 'none' role and verify that the user cannot see or interact with the post in any way." do
+  # test "adding a user with a 'read' role and verify that the user can read the post's content but not interact with it." do
 
-  end
+  # end
 
-  test "creating a post with a circle, and verify that only users within the circle can access the post according to their assigned roles." do
+  # test "adding a user with an 'interact' role and verify that the user can like and boost the post." do
 
-  end
+  # end
 
-  test "creating a post with a custom boundary, and verify that only users within the boundary can access the post according to their assigned roles." do
+  # test "adding a user with a 'participate' role and verify that the user can engage in the post's activities and discussions." do
 
-  end
+  # end
+
+  # test "adding a user with a 'caretaker' role and verify that the user can delete the post" do
+
+  # end
+
+  # test "adding a user with a 'none' role and verify that the user cannot see or interact with the post in any way." do
+
+  # end
+
+  # test "creating a post with a circle, and verify that only users within the circle can access the post according to their assigned roles." do
+
+  # end
+
+  # test "creating a post with a custom boundary, and verify that only users within the boundary can access the post according to their assigned roles." do
+
+  # end
 
 end
