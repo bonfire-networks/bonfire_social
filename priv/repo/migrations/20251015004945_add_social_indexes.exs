92df6828909bd08a515fabb69a51009a31c5a8e6
Mayel de Borniol <git@mayel.space>
2025-10-15 12:38:20 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1588

---
+++ priv/repo/migrations/20251015004945_add_social_indexes.exs
@@ -0,0 +1,14 @@
+defmodule Bonfire.Social.Repo.Migrations.AddSocialIndexes do
+  @moduledoc false
+  use Ecto.Migration
+  use Needle.Migration.Indexable
+
+  def up do
+    Bonfire.Data.Social.Replied.Migration.add_replied_indexes
+    Bonfire.Data.Social.Created.Migration.add_creator_index
+    Bonfire.Data.Social.Profile.Migration.add_profile_indexes
+
+  end
+
+  def down, do: nil
+end
