131d6de8ce0774f7460e0ffede2aba803c8df851
Mayel de Borniol <git@mayel.space>
2026-01-13 21:02:52 +0000

    fix

--- lib/media.ex
+++ lib/media.ex
@@ -447,8 +447,13 @@ defmodule Bonfire.Social.Media do
 
   def preload_newest_activity(edges) when is_list(edges) do
     repo().maybe_preload(edges, [:newest_activity])
-    |> Enum.map(fn %{newest_activity: newest_activity} = edge ->
-      %{edge | activity: newest_activity, newest_activity: nil}
-    end)
+    |> Enum.map(fn 
+      %{newest_activity: newest_activity} = edge ->
+        %{edge | activity: newest_activity, newest_activity: nil}
+      edge ->
+        warn("No :newest_activity assoc to preload")
+        edge
+      end
+    )
   end
 end
