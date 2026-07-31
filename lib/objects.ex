83b803fd21a81f421ad0c703cf77d561b8b625b8
Mayel de Borniol <git@mayel.space>
2026-07-31 16:30:12 +0100

    transfer profile to another account capabality for admins

--- lib/objects.ex
+++ lib/objects.ex
@@ -1304,46 +1304,6 @@ defmodule Bonfire.Social.Objects do
   end
 
   @doc """
-  Gets the permalink for an object.
-
-  ## Examples
-
-      iex> permalink(%{canonical_uri: "https://example.com/object/123"})
-      "https://example.com/object/123"
-
-  """
-  def permalink(%{canonical_uri: permalink}) do
-    permalink
-  end
-
-  def permalink(%{peered: %{canonical_uri: permalink}}) do
-    permalink
-  end
-
-  def permalink(%{peered: _} = object) do
-    warn("FIXME: Peered should already come preloaded in object")
-
-    object
-    # |> repo().maybe_preload(:peered)
-    |> e(:peered, :canonical_uri, nil)
-  end
-
-  def permalink(object) when is_map(object) or is_binary(object) do
-    warn(object, "FIXME: object does not have a :peered assoc, query it instead")
-
-    Utils.maybe_apply(
-      Bonfire.Federate.ActivityPub.Peered,
-      :get_canonical_uri,
-      [object]
-    )
-  end
-
-  def permalink(other) do
-    debug(other, "seems to be a local object")
-    nil
-  end
-
-  @doc """
   Finds a Bonfire object by its ActivityPub URI or object struct, even if the AP object has been pruned from cache.
 
   Tries multiple patterns to extract pointer_id:
